b7d79c16f45776d241e3b1f49f255942d15bbb53
Lo Khanh <khanlo@ethz.ch>
2026-03-13 20:04:47 +0100

    fix gds error

--- src/project.v
+++ src/project.v
@@ -46,49 +46,56 @@ module tt_um_lkhanh_vga_trng (
       .vpos(pix_y)
   );
 
-  // always @(*) begin
-  //   case (ui_in[3:2])
-  //     2'd0: begin
-  //       R = i_vga_stripe.R;
-  //       G = i_vga_stripe.G;
-  //       B = i_vga_stripe.B;
-  //     end
-  //     2'd1: begin
-  //       R = i_vga_gamepad.R;
-  //       G = i_vga_gamepad.G;
-  //       B = i_vga_gamepad.B;
-  //     end
-  //     2'd2: begin
-  //       R = i_vga_checker.R;
-  //       G = i_vga_checker.G;
-  //       B = i_vga_checker.B;
-  //     end
-  //     2'd3: begin
-  //       R = i_vga_ring.R;
-  //       G = i_vga_ring.G;
-  //       B = i_vga_ring.B;
-  //     end
-  //     default: begin
-  //       R = i_vga_stripe.R;
-  //       G = i_vga_stripe.G;
-  //       B = i_vga_stripe.B;
-  //     end
-  //   endcase
-  // end
+  wire [1:0] r_stripe, g_stripe, b_stripe;
+  wire [1:0] r_gamepad, g_gamepad, b_gamepad;
+  wire [1:0] r_checker, g_checker, b_checker;
+  wire [1:0] r_ring, g_ring, b_ring;
 
   wire [1:0] sel = ui_in[3:2];
-  assign R = (sel == 2'd0) ? i_vga_stripe.R : 
-             (sel == 2'd1) ? i_vga_gamepad.R :
-             (sel == 2'd2) ? i_vga_checker.R : 
-             i_vga_ring.R;
-  assign G = (sel == 2'd0) ? i_vga_stripe.G : 
-             (sel == 2'd1) ? i_vga_gamepad.G :
-             (sel == 2'd2) ? i_vga_checker.G : 
-             i_vga_ring.R;
-  assign B = (sel == 2'd0) ? i_vga_stripe.B : 
-             (sel == 2'd1) ? i_vga_gamepad.B :
-             (sel == 2'd2) ? i_vga_checker.B : 
-             i_vga_ring.B;
+  reg [1:0] r_o, g_o, b_o;
+  assign {R, G, B} = {r_o, g_o, b_o};
+
+  always @(*) begin
+    case (sel)
+      2'd0: begin
+        r_o = r_stripe;
+        g_o = g_stripe;
+        b_o = b_stripe;
+      end
+      2'd1: begin
+        r_o = r_gamepad;
+        g_o = g_gamepad;
+        b_o = b_gamepad;
+      end
+      2'd2: begin
+        r_o = r_checker;
+        g_o = g_checker;
+        b_o = b_checker;
+      end
+      2'd3: begin
+        r_o = r_ring;
+        g_o = g_ring;
+        b_o = b_ring;
+      end
+      default: begin
+        r_o = r_stripe;
+        g_o = g_stripe;
+        b_o = b_stripe;
+      end
+    endcase
+  end
+  // assign R = (sel == 2'd0) ? i_vga_stripe.R :
+  //            (sel == 2'd1) ? i_vga_gamepad.R :
+  //            (sel == 2'd2) ? i_vga_checker.R : 
+  //            i_vga_ring.R;
+  // assign G = (sel == 2'd0) ? i_vga_stripe.G :
+  //            (sel == 2'd1) ? i_vga_gamepad.G :
+  //            (sel == 2'd2) ? i_vga_checker.G : 
+  //            i_vga_ring.G;
+  // assign B = (sel == 2'd0) ? i_vga_stripe.B :
+  //            (sel == 2'd1) ? i_vga_gamepad.B :
+  //            (sel == 2'd2) ? i_vga_checker.B :
+  //            i_vga_ring.B;
 
   vga_checker i_vga_checker (
       .ui_in,
@@ -98,9 +105,9 @@ module tt_um_lkhanh_vga_trng (
       .rst_n,
       .video_active,
       .vsync,
-      .R(),
-      .G(),
-      .B()
+      .R(r_checker),
+      .G(g_checker),
+      .B(b_checker)
   );
 
   vga_ring i_vga_ring (
@@ -111,9 +118,9 @@ module tt_um_lkhanh_vga_trng (
       .clk,
       .rst_n,
       .video_active,
-      .R(),
-      .G(),
-      .B()
+      .R(r_ring),
+      .G(g_ring),
+      .B(b_ring)
   );
 
   vga_stripe i_vga_stripe (
@@ -124,9 +131,9 @@ module tt_um_lkhanh_vga_trng (
       .rst_n,
       .video_active,
       .vsync,
-      .R(),
-      .G(),
-      .B()
+      .R(r_stripe),
+      .G(g_stripe),
+      .B(b_stripe)
   );
 
   vga_gamepad i_vga_gamepad (
@@ -136,9 +143,9 @@ module tt_um_lkhanh_vga_trng (
       .clk,
       .rst_n,
       .video_active,
-      .R(),
-      .G(),
-      .B()
+      .R(r_gamepad),
+      .G(g_gamepad),
+      .B(b_gamepad)
   );
 
 endmodule
